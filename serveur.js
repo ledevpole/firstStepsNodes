ddc5bf02c988d52fe72153398e04d7cde16d95e1
ledevpole <ledevpole@gmail.com>
2018-07-13 10:09:06 +0200

    routing sur express.js

--- serveur.js
+++ serveur.js
@@ -10,30 +10,30 @@ app.get('/', function(req, res) {
 
     res.send('Vous êtes à l\'accueil, que puis-je pour vous ?');
 
-});
+})
 
 
-app.get('/sous-sol', function(req, res) {
+.get('/sous-sol', function(req, res) {
 
     res.setHeader('Content-Type', 'text/plain');
 
     res.send('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
 
-});
+})
 
 
-app.get('/etage/:etagenum/chambre', function(req, res) {
+.get('/etage/:etagenum/chambre', function(req, res) {
 
     res.setHeader('Content-Type', 'text/plain');
 
     res.end('Vous êtes à la chambre de l\'étage n°' + req.params.etagenum);
 
-});
+})
 
 
 // ... Tout le code de gestion des routes (app.get) se trouve au-dessus
 
-app.use(function(req, res, next){
+.use(function(req, res, next){
 
     res.setHeader('Content-Type', 'text/plain');
 
